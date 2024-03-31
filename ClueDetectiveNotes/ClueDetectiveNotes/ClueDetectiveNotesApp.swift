75612db9a7f68aadd346cc6d19dd722ed79d1b1e
DasanKim <tempzipp@gmail.com>
2024-03-31 13:38:37 +0900

    ✨Feat: 프로젝트 생성

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/ClueDetectiveNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClueDetectiveNotesApp.swift
+//  ClueDetectiveNotes
+//
+//  Created by Yena on 2024/03/31.
+//
+
+import SwiftUI
+
+@main
+struct ClueDetectiveNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
