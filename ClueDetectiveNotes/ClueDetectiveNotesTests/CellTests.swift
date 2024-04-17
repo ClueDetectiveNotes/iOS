dd4746ce1758f87edec1e1105ad3fd30afff8788
DasanKim <tempzipp@gmail.com>
2024-04-18 00:17:37 +0900

    ✅Test: Cell Tests 생성 및 Cell Case 추가
    - mainMarker를 설정하면 해당 mainMarker가 추가된다.
    - subMarker를 설정하면 해당 subMarker가 추가된다.

---
+++ ClueDetectiveNotes/ClueDetectiveNotesTests/CellTests.swift
@@ -0,0 +1,52 @@
+//
+//  CellTests.swift
+//  ClueDetectiveNotesTests
+//
+//  Created by Yena on 2024/04/17.
+//
+
+import XCTest
+@testable import ClueDetectiveNotes
+
+final class CellTests: XCTestCase {
+    private var cell: Cell!
+
+    override func setUpWithError() throws {
+        cell = Cell(
+            rowName: RowName(card: Edition.classic.cards.suspects.first!),
+            colName: ColName(player: DummyPlayers.players.first!)
+        )
+    }
+
+    override func tearDownWithError() throws {
+        cell = nil
+    }
+    
+    // mainMarker를 설정하면 해당 mainMarker가 추가된다.
+    func test_addMainMarker() {
+        let mainMarker = MainMarker(notation: .question)
+        cell.setMainMarker(mainMarker)
+        
+        let expectation = cell.getMainMarker()
+        
+        XCTAssertEqual(mainMarker, expectation)
+    }
+    
+    // subMarker를 설정하면 해당 subMarker가 추가된다.
+    func test_addSubMarker() {
+        let subMarker = SubMarker(notation: "a")
+        cell.setSubMarker(subMarker)
+        
+        let expectation = cell.getSubMarkers()
+        XCTAssertTrue(expectation.contains(subMarker))
+    }
+
+//    2. 이미 설정된 marker를 다시 설정하면 해당 marker가 삭제된다.
+//    → removeMarkerIfAlreadySet
+
+//    3. main marker가 있는 상태에서 main marker가 들어오면 새로운 값으로 override된다.
+//    → overrideMainMarkerIfAlreadyExists
+
+//    4. sub marker가 있는 상태에서 sub marker가 들어오면 markers에 추가된다.
+//    → addSubMarkerIfAlreadyExists
+}
