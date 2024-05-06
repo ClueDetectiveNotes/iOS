d2eae11954349b181f563746febf312ee660c13d
MaryJo-github <jotjdal0215@gmail.com>
2024-05-06 17:26:42 +0900

    ♻️Refactor: Sheet 모델 재생성 및 메서드 정리
    - SheetError case 추가

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Model/Sheet/Errors/SheetError.swift
@@ -0,0 +1,34 @@
+//
+//  SheetError.swift
+//  ClueDetectiveNotes
+//
+//  Created by Yena on 2024/04/15.
+//
+
+import Foundation
+
+enum SheetError: LocalizedError {
+    case cellNotFound
+    case notMultiSelectionMode
+    case cannotSelectAlreadySelectedCell
+    case cannotUnselectNeverChosenCell
+    case notYetSelectAnyRowName
+    case notYetSelectAnyColumnName
+    
+    var errorDescription: String? {
+        switch self {
+        case .cellNotFound:
+            return "Can not find that cell in cells"
+        case .notMultiSelectionMode:
+            return "Current selection mode is not multi selection mode"
+        case .cannotSelectAlreadySelectedCell:
+            return "Can not select already selected cell"
+        case .cannotUnselectNeverChosenCell:
+            return "Can not unselect never chosen cell"
+        case .notYetSelectAnyRowName:
+            return "Not yet select any row name"
+        case .notYetSelectAnyColumnName:
+            return "Not yet select any column name"
+        }
+    }
+}
---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Model/Sheet/Sheet.swift
@@ -0,0 +1,216 @@
+//
+//  Sheet.swift
+//  ClueDetectiveNotes
+//
+//  Created by Dasan & Mary on 2024/04/13.
+//
+
+import Foundation
+
+struct Sheet {
+    private var cells = [Cell]()
+    private var isMultiMode: Bool
+    private var rowNames = [RowName]()
+    private var colNames = [ColName]()
+    private var selectedCells = [Cell]()
+    private var selectedRowNames = [CardType: RowName]()
+    private var selectedColName: ColName?
+    
+    init(
+        players: [Player],
+        cards: Cards,
+        isMultiMode: Bool = false
+    ) {
+        self.isMultiMode = isMultiMode
+        
+        players.forEach { player in
+            colNames.append(ColName(player: player))
+        }
+
+        cards.allCards().forEach { card in
+            rowNames.append(RowName(card: card))
+        }
+
+        for rowName in rowNames {
+            for colName in colNames {
+                let cell = Cell(
+                    rowName: rowName,
+                    colName: colName
+                )
+                cells.append(cell)
+            }
+        }
+    }
+    
+    func getCells() -> [Cell] {
+        return cells
+    }
+    
+    func getRowNames() -> [RowName] {
+        return rowNames
+    }
+    
+    func getColNames() -> [ColName] {
+        return colNames
+    }
+    
+    func getSelectedCells() -> [Cell] {
+        return selectedCells
+    }
+    
+    func getSelectedRowNames() -> [CardType:RowName] {
+        return selectedRowNames
+    }
+    
+    func getSelectedColName() -> ColName? {
+        return selectedColName
+    }
+    
+    func hasSelectedCell() -> Bool {
+        return !selectedCells.isEmpty
+    }
+    
+    func hasSelectedRowName() -> Bool {
+        return !selectedRowNames.isEmpty
+    }
+    
+    func hasSelectedColName() -> Bool {
+        return selectedColName != nil
+    }
+    
+    func isSelectedRowName(_ rowName: RowName) -> Bool {
+        return selectedRowNames.values.contains(rowName)
+    }
+    
+    func isSelectedColName(_ colName: ColName) -> Bool {
+        return selectedColName == colName
+    }
+    
+    func isMultiSelectionMode() -> Bool {
+        return isMultiMode
+    }
+    
+    mutating func selectCell(_ cell: Cell) -> [Cell] {
+        selectedCells.append(cell)
+        
+        return selectedCells
+    }
+    
+    mutating func selectCell(rowName: RowName, colName: ColName) throws -> [Cell] {
+        let selectedCell = try findCell(rowName: rowName, colName: colName)
+        
+        selectedCells.append(selectedCell)
+        
+        return selectedCells
+    }
+    
+    private func findCell(rowName: RowName, colName: ColName) throws -> Cell {
+        for cell in cells {
+            if cell.getRowName() == rowName,
+               cell.getColName() == colName {
+                return cell
+            }
+        }
+
+        throw SheetError.cellNotFound
+    }
+    
+    mutating func unselectCell() {
+        selectedCells.removeAll()
+    }
+    
+    mutating func switchSelectionMode() {
+        if isMultiSelectionMode(), hasSelectedCell() {
+            unselectCell()
+        }
+        
+        isMultiMode.toggle()
+    }
+    
+    mutating func multiSelectCell(rowName: RowName, colName: ColName) throws -> [Cell] {
+        guard isMultiSelectionMode() else { throw SheetError.notMultiSelectionMode }
+        guard try !isSelectedCell(rowName: rowName, colName: colName) else {
+            throw SheetError.cannotSelectAlreadySelectedCell
+        }
+        
+        let cell = try findCell(rowName: rowName, colName: colName)
+        selectedCells.append(cell)
+
+        return selectedCells
+    }
+    
+    mutating func multiUnselectCell(rowName: RowName, colName: ColName) throws -> [Cell] {
+        guard isMultiSelectionMode() else { throw SheetError.notMultiSelectionMode }
+        guard try isSelectedCell(rowName: rowName, colName: colName) else {
+            throw SheetError.cannotUnselectNeverChosenCell
+        }
+        
+        let cell = try findCell(rowName: rowName, colName: colName)
+        if let index = selectedCells.firstIndex(where: { $0.id == cell.id }) {
+            selectedCells.remove(at: index)
+        }
+        
+        return selectedCells
+    }
+    
+    func isSelectedCell(_ cell: Cell) -> Bool {
+        return selectedCells.contains(where: { $0.id == cell.id })
+    }
+    
+    func isSelectedCell(rowName: RowName, colName: ColName) throws -> Bool {
+        let cell = try findCell(rowName: rowName, colName: colName)
+        return selectedCells.contains(where: { $0.id == cell.id })
+    }
+
+    mutating func selectRowName(_ rowName: RowName) -> [Cell] {
+        let type = rowName.card.type
+        
+        selectedRowNames[type] = rowName
+
+        return cells.filter { cell in
+            cell.getRowName() == rowName
+        }
+    }
+    
+    mutating func unselectRowName(_ rowName: RowName) {
+        let type = rowName.card.type
+        
+        selectedRowNames[type] = nil
+    }
+    
+    mutating func selectColumnName(_ colName: ColName) -> [Cell] {
+        selectedColName = colName
+        
+        return cells.filter { cell in
+            cell.getColName() == colName
+        }
+    }
+    
+    mutating func unselectColumnName() {
+        selectedColName = nil
+    }
+    
+    func getCellsIntersectionOfSelection() throws -> [Cell] {
+        let rowNames = selectedRowNames.values
+        let colName = selectedColName
+        
+        guard let colName else {
+            throw SheetError.notYetSelectAnyColumnName
+        }
+        guard !rowNames.isEmpty else {
+            throw SheetError.notYetSelectAnyRowName
+        }
+        
+        return cells.filter { cell in
+            rowNames.contains(cell.getRowName()) && cell.getColName() == colName
+        }
+    }
+}
+
+struct RowName: Hashable {
+    let card: ClueCard
+}
+
+struct ColName: Hashable {
+    let player: Player
+}
