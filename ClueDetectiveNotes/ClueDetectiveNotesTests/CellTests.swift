73bc9a595d1cd8fb0d133b1144312ec4419e6d57
DasanKim <tempzipp@gmail.com>
2024-05-07 14:49:45 +0900

    ✅Test: Cell 메서드 변경으로 인한 Test 수정 및 추가

--- ClueDetectiveNotes/ClueDetectiveNotesTests/CellTests.swift
+++ ClueDetectiveNotes/ClueDetectiveNotesTests/CellTests.swift
@@ -2,7 +2,7 @@
 //  CellTests.swift
 //  ClueDetectiveNotesTests
 //
-//  Created by Yena on 2024/04/17.
+//  Created by Dasan & Mary on 2024/04/17.
 //
 
 import XCTest
@@ -35,57 +35,79 @@ final class CellTests: XCTestCase {
     // subMarker를 설정하면 해당 subMarker가 추가된다.
     func test_addSubMarker() {
         let subMarker = SubMarker(notation: "a")
-        cell.setSubMarker(subMarker)
+        try! cell.setSubMarker(subMarker)
         
         let expectation = cell.getSubMarkers()
         
         XCTAssertTrue(expectation.contains(subMarker))
     }
-
-    // 이미 설정된 mainMarker를 다시 설정하면 해당 mainMarker가 삭제된다.
+    
+    // 설정된 mainMarker를 삭제하면 mainMarKer는 nil이 된다.
     func test_removeMainMarkerIfAlreadySet() {
         let mainMarker = MainMarker(notation: .question)
         cell.setMainMarker(mainMarker)
-        cell.setMainMarker(mainMarker)
         
+        cell.removeMainMarker()
         let expectation = cell.getMainMarker()
         
         XCTAssertNil(expectation)
     }
     
-    // 이미 설정된 subMarker를 다시 설정하면 해당 subMarker가 삭제된다.
+    // 설정된 subMarker를 삭제하면 해당 subMarker는 삭제된다.
     func test_removeSubMarkerIfAlreadySet() {
         let subMarker = SubMarker(notation: "a")
-        cell.setSubMarker(subMarker)
-        cell.setSubMarker(subMarker)
+        try! cell.setSubMarker(subMarker)
         
+        try! cell.removeSubMarker(subMarker)
         let expectation = cell.getSubMarkers()
-        
+
         XCTAssertFalse(expectation.contains(subMarker))
     }
-
-    // main marker가 있는 상태에서 main marker가 들어오면 새로운 값으로 override된다.
-    func test_overrideMainMarkerIfAlreadyExists() {
-        let previousMainMarker = MainMarker(notation: .question)
-        let mainMarker = MainMarker(notation: .exclamation)
-        cell.setMainMarker(previousMainMarker)
-        cell.setMainMarker(mainMarker)
-        
-        let expectation = cell.getMainMarker()
-        
-        XCTAssertEqual(mainMarker, expectation)
-    }
-
-    // sub marker가 있는 상태에서 sub marker가 들어오면 markers에 추가된다.
-    func test_addSubMarkerIfAlreadyExists() {
-        let previousSubMarker = SubMarker(notation: "a")
-        let subMarker = SubMarker(notation: "b")
-        cell.setSubMarker(previousSubMarker)
-        cell.setSubMarker(subMarker)
-        
-        let expectation = cell.getSubMarkers()
-        
-        XCTAssertTrue(expectation.contains(previousSubMarker))
-        XCTAssertTrue(expectation.contains(subMarker))
-    }
+    
+//    // 이미 설정된 mainMarker를 다시 설정하면 해당 mainMarker가 삭제된다.
+//    func test_removeMainMarkerIfAlreadySet() {
+//        let mainMarker = MainMarker(notation: .question)
+//        cell.setMainMarker(mainMarker)
+//        cell.setMainMarker(mainMarker)
+//        
+//        let expectation = cell.getMainMarker()
+//        
+//        XCTAssertNil(expectation)
+//    }
+//    
+//    // 이미 설정된 subMarker를 다시 설정하면 해당 subMarker가 삭제된다.
+//    func test_removeSubMarkerIfAlreadySet() {
+//        let subMarker = SubMarker(notation: "a")
+//        cell.setSubMarker(subMarker)
+//        cell.setSubMarker(subMarker)
+//        
+//        let expectation = cell.getSubMarkers()
+//        
+//        XCTAssertFalse(expectation.contains(subMarker))
+//    }
+//
+//    // main marker가 있는 상태에서 main marker가 들어오면 새로운 값으로 override된다.
+//    func test_overrideMainMarkerIfAlreadyExists() {
+//        let previousMainMarker = MainMarker(notation: .question)
+//        let mainMarker = MainMarker(notation: .exclamation)
+//        cell.setMainMarker(previousMainMarker)
+//        cell.setMainMarker(mainMarker)
+//        
+//        let expectation = cell.getMainMarker()
+//        
+//        XCTAssertEqual(mainMarker, expectation)
+//    }
+//
+//    // sub marker가 있는 상태에서 sub marker가 들어오면 markers에 추가된다.
+//    func test_addSubMarkerIfAlreadyExists() {
+//        let previousSubMarker = SubMarker(notation: "a")
+//        let subMarker = SubMarker(notation: "b")
+//        cell.setSubMarker(previousSubMarker)
+//        cell.setSubMarker(subMarker)
+//        
+//        let expectation = cell.getSubMarkers()
+//        
+//        XCTAssertTrue(expectation.contains(previousSubMarker))
+//        XCTAssertTrue(expectation.contains(subMarker))
+//    }
 }
