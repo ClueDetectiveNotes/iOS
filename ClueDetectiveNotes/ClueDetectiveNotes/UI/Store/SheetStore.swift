293cc37acf02dad021fe4e58187c69abcbf9344a
MaryJo-github <jotjdal0215@gmail.com>
2024-05-13 17:21:10 +0900

    ♻️Refactor: SheetStore 수정 및 불필요한 Store 삭제

--- ClueDetectiveNotes/ClueDetectiveNotes/UI/Store/SheetStore.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/UI/Store/SheetStore.swift
@@ -8,38 +8,14 @@
 import Foundation
 
 final class SheetStore: ObservableObject {
-    @Published var selectedCells = [Cell]()
-    @Published var isMultiMode: Bool = false
+    @Published var sheet: Sheet
+    @Published var isDisplayMarkerControlBar: Bool
     
-    private var loadSheetUseCase = LoadSheetUseCase()
-    private var clickCellUseCase = ClickCellUseCase()
-    private var longClickCellUseCase = LongClickCellUseCase()
-    
-    func onLoadSheet() -> Sheet {
-        do {
-            let state = try loadSheetUseCase.execute()
-            let sheet = state["sheet"] as! Sheet
-            selectedCells = sheet.getSelectedCells()
-            isMultiMode = sheet.isMultiSelectionMode()
-            
-            return sheet
-        } catch {
-            return Sheet(players: [], cards: Edition.classic.cards)
-        }
-    }
-    
-    func onClickCell(_ cell: Cell) {
-        do {
-            let state = try clickCellUseCase.execute(cell: cell)
-            selectedCells = state["selectedCells"] as! [Cell]
-        } catch {
-            
-        }
-    }
-    
-    func onLongClickCell(_ cell: Cell) {
-        let state = longClickCellUseCase.execute(cell: cell)
-        selectedCells = state["selectedCells"] as! [Cell]
-        isMultiMode = state["isMultiSelectionMode"] as! Bool
+    init(
+        sheet: Sheet = GameSetter.shared.getSheetInstance(),
+        isDisplayMarkerControlBar: Bool = false
+    ) {
+        self.sheet = sheet
+        self.isDisplayMarkerControlBar = isDisplayMarkerControlBar
     }
 }
