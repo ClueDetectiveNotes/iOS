46dde7723b890c250a83c6449859787a2fd36a43
DasanKim <tempzipp@gmail.com>
2024-06-05 21:34:19 +0900

    🩹Chore: SheetMode 타입 description 추가

--- ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Sheet/SheetMode.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Sheet/SheetMode.swift
@@ -5,6 +5,19 @@
 //  Created by Dasan & Mary on 5/25/24.
 //
 
-enum SheetMode {
+enum SheetMode: CustomStringConvertible {
     case single, multi, inference, preInference
+    
+    var description: String {
+        switch self {
+        case .single:
+            return "single"
+        case .multi:
+            return "multi"
+        case .inference:
+            return "inference"
+        case .preInference:
+            return "preInference"
+        }
+    }
 }
