90c3bf4fc224b908db940461a970b573ccf649c3
DasanKim <tempzipp@gmail.com>
2024-06-23 11:26:46 +0900

    ✨Feat: Setting 타입 수정 및 메서드 추가
    - getPlayers, getPlayersWithoutSolution 메서드 추가
    - getPlayerCount, getPlayersCountWithoutSolution 메서드 추가
    - getMinPlayerCount, getMaxPlayerCount 메서드 추가
    - addPlayer 메서드 생성 및 구현
    - isDuplicatePlayer 메서드 생성 및 구현

--- ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Game/Setting.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Game/Setting.swift
@@ -27,8 +27,30 @@ final class Setting {
         self.subMarkerTypes = subMarkerTypes
     }
     
-    func getPlayers() -> [Player] {
+    func getPlayers() -> [any Player] {
+        return players.sorted { $0.id < $1.id }
+    }
+    
+    func getPlayersWithoutSolution() -> [any Player] {
         return players
+            .filter { !($0 is Solution) }
+            .sorted { $0.id < $1.id }
+    }
+    
+    func getPlayerCount() -> Int {
+        return players.count
+    }
+    
+    func getPlayersCountWithoutSolution() -> Int {
+        return players.filter { !($0 is Solution) }.count
+    }
+    
+    func getMinPlayerCount() -> Int {
+        return playerCountRange.lowerBound
+    }
+    
+    func getMaxPlayerCount() -> Int {
+        return playerCountRange.upperBound
     }
     
     func getEdition() -> Edition {
@@ -53,7 +75,43 @@ final class Setting {
         subMarkerTypes.append(SubMarker(notation: notation))
     }
     
+    func addPlayer(id: Int, name: String, type: PlayerType) throws {
+        let trimmingName = name.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard !trimmingName.isEmpty else {
+            throw SettingError.nameIsEmpty
+        }
+        
+        guard !isDuplicatePlayer(id: id, name: name) else {
+            throw SettingError.alreadyExistsPlayer
+        }
+        
+        var player: any Player
+        
+        switch type {
+        case .user:
+            player = User(id: id, name: name)
+        case .other:
+            player = Other(id: id, name: name)
+        case .solution:
+            player = Solution(id: id, name: name)
+        }
+        
+        players.append(player)
+    }
+}
+
+extension Setting {
     private func isValidSubMarkerNotation(_ notation: String) -> Bool {
         return !notation.isEmpty && notation.count <= 2
+    }
+    
+    private func isDuplicatePlayer(id: Int, name: String) -> Bool {
+        for player in players {
+            if player.id == id || player.name == name {
+                return true
+            }
+        }
+        return false
     }
 }
