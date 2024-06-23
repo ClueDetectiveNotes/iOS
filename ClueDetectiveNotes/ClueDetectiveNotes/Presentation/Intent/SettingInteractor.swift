6940930863fd2b4768c62004f04b2ea96018f326
DasanKim <tempzipp@gmail.com>
2024-06-23 11:33:40 +0900

    ✨Feat: 플레이어 세팅과 관련된 메서드들 추가
    - clickMinusButton, clickPlusButton, clickPlayerSettingNextButton 메서드 생성 및 구현
    - selectPlayer, clickPlayerDetailSettingNextButton 메서드 생성 및 구현

--- ClueDetectiveNotes/ClueDetectiveNotes/Presentation/Intent/SettingInteractor.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/Intent/SettingInteractor.swift
@@ -10,13 +10,16 @@ import OSLog
 struct SettingInteractor {
     private var settingStore: SettingStore
     private let addSubMarkerTypeUseCase: AddSubMarkerTypeUseCase
+    private let createPlayersUseCase: CreatePlayersUseCase
     
     init(
         settingStore: SettingStore,
-        addSubMarkerTypeUseCase: AddSubMarkerTypeUseCase = AddSubMarkerTypeUseCase()
+        addSubMarkerTypeUseCase: AddSubMarkerTypeUseCase = AddSubMarkerTypeUseCase(),
+        createPlayersUseCase: CreatePlayersUseCase = CreatePlayersUseCase()
     ) {
         self.settingStore = settingStore
         self.addSubMarkerTypeUseCase = addSubMarkerTypeUseCase
+        self.createPlayersUseCase = createPlayersUseCase
     }
     
     func addSubMarker(_ marker: SubMarker) {
@@ -25,6 +28,65 @@ struct SettingInteractor {
             updateSettingStore(presentationSetting: presentationSetting)
         } catch {
             os_log("%{public}@", type: .default, error.localizedDescription)
+        }
+    }
+    
+    func clickMinusButton() {
+        let minPlayerCount = GameSetter.shared.getSetting().getMinPlayerCount()
+        
+        if settingStore.count > minPlayerCount {
+            settingStore.decrementCount()
+            //settingStore.removeLastPlayerName()
+            var temp = settingStore.playerNames
+            temp.removeLast()
+            settingStore.playerNames = temp
+        }
+        
+        if settingStore.count == minPlayerCount {
+            settingStore.setIsDisabledMinusButton(true)
+        } else {
+            settingStore.setIsDisabledMinusButton(false)
+            settingStore.setIsDisabledPlusButton(false)
+        }
+    }
+    
+    func clickPlusButton() {
+        let maxPlayerCount = GameSetter.shared.getSetting().getMaxPlayerCount()
+        
+        if settingStore.count <  maxPlayerCount {
+            settingStore.incrementCount()
+            settingStore.appendPlayerName("")
+        }
+        
+        if settingStore.count == maxPlayerCount {
+            settingStore.setIsDisabledPlusButton(true)
+        } else {
+            settingStore.setIsDisabledMinusButton(false)
+            settingStore.setIsDisabledPlusButton(false)
+        }
+    }
+    
+    func clickPlayerSettingNextButton() {
+        if settingStore.isValidPlayerNames() {
+            let trimmingNames = settingStore.trimmingPlayerNames()
+            settingStore.overwritePlayerNames(trimmingNames)
+        }
+    }
+    
+    func selectPlayer(_ playerName: String) {
+        settingStore.setSelectedPlayer(playerName)
+    }
+    
+    func clickPlayerDetailSettingNextButton() {
+        do {
+            let playerNames = settingStore.playerNames
+            let userName = settingStore.selectedPlayer
+            
+            let presentationSetting = try createPlayersUseCase.execute(playerNames, userName)
+            
+            updateSettingStore(presentationSetting: presentationSetting)
+        } catch {
+            
         }
     }
 }
