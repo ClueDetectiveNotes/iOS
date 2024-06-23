40c18b88f261d23da56655d72e373fd4d24d00cd
DasanKim <tempzipp@gmail.com>
2024-06-23 11:24:51 +0900

    ✨Feat: SettingError 케이스 추가

--- ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Game/Errors/SettingError.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Game/Errors/SettingError.swift
@@ -10,6 +10,9 @@ import Foundation
 enum SettingError: LocalizedError {
     case alreadyExistsSubMarkerType
     case invalidSubMarkerType
+    case playerCountOutOfRange
+    case alreadyExistsPlayer
+    case nameIsEmpty
     
     var errorDescription: String? {
         switch self {
@@ -17,6 +20,12 @@ enum SettingError: LocalizedError {
             return "The sub marker type already exists."
         case .invalidSubMarkerType:
             return "The sub marker type is invalid"
+        case .playerCountOutOfRange:
+            return "Number of Player exceeds range"
+        case .alreadyExistsPlayer:
+            return "There already exists a player with the same name or ID."
+        case .nameIsEmpty:
+            return "An player name is empty."
         }
     }
 }
