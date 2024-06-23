e9b54808cfaabd8da19afb68efe34eae7acdd9a3
DasanKim <tempzipp@gmail.com>
2024-06-23 11:34:45 +0900

    ✨Feat: 세팅 화면에서 사용될 TitleView 생성

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/View/TitleView.swift
@@ -0,0 +1,49 @@
+//
+//  TitleView.swift
+//  ClueDetectiveNotes
+//
+//  Created by Dasan & Mary on 6/20/24.
+//
+
+import SwiftUI
+
+struct TitleView: View {
+    private let title: String
+    private let description: String
+    
+    init(
+        title: String,
+        description: String
+    ) {
+        self.title = title
+        self.description = description
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            HStack {
+                Text(title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            
+            Spacer()
+                .frame(height: 10)
+            
+            HStack {
+                Text(description)
+                    .foregroundStyle(.gray)
+                Spacer()
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TitleView(
+        title: "Title",
+        description: "설명입니다."
+    )
+}
