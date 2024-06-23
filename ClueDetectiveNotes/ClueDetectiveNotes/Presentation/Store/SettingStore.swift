3fa99eaf7b55c6f3c63687cf83d796b8e0944e1e
DasanKim <tempzipp@gmail.com>
2024-06-23 11:32:14 +0900

    ♻️Refactor: 플레이어 세팅을 위한 SettingStore 내 프로퍼티 및 매서드 추가
    - 세팅 화면에서만 사용되는 count, playerNames, selectedPlayer 프로퍼티 생성
    - isDisabledMinusButton, isDisabledPlusButton 프로퍼티 생성
    - count, playerNames와 관련된 메서드 추가
    - 입력된 이름의 유효성 검사하는 메서드들 추가

--- ClueDetectiveNotes/ClueDetectiveNotes/Presentation/Store/SettingStore.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/Store/SettingStore.swift
@@ -9,12 +9,101 @@ import Foundation
 
 final class SettingStore: ObservableObject {
     @Published private(set) var setting: PresentationSetting
-
-    init() {
+    @Published private(set) var count: Int
+    @Published var playerNames: [String]
+    
+    @Published private(set) var isDisabledMinusButton: Bool
+    @Published private(set) var isDisabledPlusButton: Bool
+    
+    @Published private(set) var selectedPlayer: String
+    
+    
+    init(
+        isDisabledMinusButton: Bool = true,
+        isDisabledPlusButton: Bool = false
+    ) {
         self.setting = ConvertManager.getImmutableSetting(GameSetter.shared.getSetting())
+        self.count = GameSetter.shared.getSetting().getMinPlayerCount()
+        self.playerNames = Array(repeating: "", count: GameSetter.shared.getSetting().getMinPlayerCount())
+        
+        self.isDisabledMinusButton = isDisabledMinusButton
+        self.isDisabledPlusButton = isDisabledPlusButton
+        
+        self.selectedPlayer = ""
     }
     
     func overwriteSetting(_ newSetting: PresentationSetting) {
         setting = newSetting
+    }
+    
+    func incrementCount() {
+        count += 1
+    }
+    
+    func decrementCount() {
+        count -= 1
+    }
+    
+    func appendPlayerName(_ name: String) {
+        playerNames.append(name)
+    }
+    
+    func removeLastPlayerName() {
+        playerNames.removeLast()
+    }
+    
+    func isValidPlayerNames() -> Bool {
+        let trimmingNames = trimmingPlayerNames()
+        
+        guard !isEmptyStrings(trimmingNames) else {
+            return false
+        }
+        
+        guard Set(trimmingNames).count == trimmingNames.count else {
+            return false
+        }
+        
+        return true
+    }
+    
+    func trimmingPlayerNames() -> [String] {
+        return playerNames.map {
+            $0.trimmingCharacters(in: .whitespaces)
+        }
+    }
+    
+    func overwritePlayerNames(_ newNames: [String]) {
+        playerNames = newNames
+    }
+    
+    func setIsDisabledMinusButton(_ value: Bool) {
+        isDisabledMinusButton = value
+    }
+    
+    func setIsDisabledPlusButton(_ value: Bool) {
+        isDisabledPlusButton = value
+    }
+    
+    func isSelectedPlayer(_ playerName: String) -> Bool {
+        return selectedPlayer == playerName
+    }
+    
+    func isEmptySelectedPlayer() -> Bool {
+        return selectedPlayer.isEmpty
+    }
+    
+    func setSelectedPlayer(_ playerName: String) {
+        selectedPlayer = playerName
+    }
+}
+
+extension SettingStore {
+    private func isEmptyStrings(_ strings: [String]) -> Bool {
+        for string in strings {
+            if string.isEmpty {
+                return true
+            }
+        }
+        return false
     }
 }
