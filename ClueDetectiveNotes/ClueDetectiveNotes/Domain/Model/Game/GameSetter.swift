9293c78043a38f3d3ed44ae4248251758c1ef4c8
DasanKim <tempzipp@gmail.com>
2024-06-23 11:27:37 +0900

    🐛Fix: GameSetter 내 getSheet 로직 수정

--- ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Game/GameSetter.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Game/GameSetter.swift
@@ -8,41 +8,25 @@
 final class GameSetter {
     static let shared = GameSetter()
     
-    private var sheet: Sheet
+    private var sheet: Sheet?
     private var setting = Setting()
     
-    private init() { 
-        self.sheet = Sheet(
-            players: setting.getPlayers(),
-            cards: setting.getEdition().cards
-        )
-    }
+    private init() { }
     
     func getSheet() -> Sheet {
-        return sheet
+        if let sheet {
+            return sheet
+        } else {
+            let newSheet = Sheet(
+                players: setting.getPlayers(),
+                cards: setting.getEdition().cards
+            )
+            self.sheet = newSheet
+            return newSheet
+        }
     }
-    
-//    func getPresentationSheet() -> PresentationSheet {
-//        return PresentationSheet(
-//            cells: sheet.getCellsImmutable(),
-//            mode: sheet.getMode(),
-//            rowNames: sheet.getRowNames(),
-//            colNames: sheet.getColNames(),
-//            selectedCells: sheet.getSelectedCellsImmutable(),
-//            selectedRowNames: sheet.getSelectedRowNames(),
-//            selectedColName: sheet.getSelectedColName()
-//        )
-//    }
     
     func getSetting() -> Setting {
         return setting
     }
-    
-//    func getPresentationSetting() -> PresentationSetting {
-//        return PresentationSetting(
-//            players: setting.getPlayers(),
-//            edition: setting.getEdition(),
-//            subMarkerTypes: setting.getSubMarkerTypes()
-//        )
-//    }
 }
