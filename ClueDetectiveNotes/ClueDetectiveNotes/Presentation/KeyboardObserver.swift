18d03d1ed693a09b1d4e576ee36d5e2a8e73d728
DasanKim <tempzipp@gmail.com>
2024-07-03 16:19:48 +0900

    ✨Feat: KeyboardObserver 생성

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/KeyboardObserver.swift
@@ -0,0 +1,42 @@
+//
+//  KeyboardObserver.swift
+//  ClueDetectiveNotes
+//
+//  Created by Dasan on 7/3/24.
+//
+
+import SwiftUI
+
+final class KeyboardObserver: ObservableObject {
+    @Published private(set) var isKeyboardVisible: Bool = false
+    
+    init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyBoardWillShow(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc private func keyBoardWillShow(notification: Notification) {
+        if let keyboardSize = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+            isKeyboardVisible = true
+        }
+    }
+    
+    @objc private func keyboardWillHide(notification: Notification) {
+        isKeyboardVisible = false
+    }
+}
