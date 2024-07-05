5e8eccd6c737e1377996d1a2a2067c0d8a4279a8
DasanKim <tempzipp@gmail.com>
2024-07-05 21:49:53 +0900

    🩹Chore: Component 그룹 생성 및 파일 그룹 정리
    - EmptyView, TitleView를 Component 그룹으로 이동
    - PlayerSettingView, PlayerDetailSettingView 그룹 내 순서 수정

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/View/Component/EmptyView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyView.swift
+//  ClueDetectiveNotes
+//
+//  Created by Dasan on 7/5/24.
+//
+
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+#Preview {
+    EmptyView()
+}
