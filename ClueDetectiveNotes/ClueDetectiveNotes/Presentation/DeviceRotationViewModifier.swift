2b803281391c14b84aa391879cbacee1686aa7ab
DasanKim <tempzipp@gmail.com>
2024-07-09 13:23:03 +0900

    ✨Feat: 기기 회전 감지하는 DeviceRotationViewModifier 생성
    - 회전 4 방향 모두 감지할 수 있도록 설정
    - 그 외 실기기 테스트를 위한 설정

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/DeviceRotationViewModifier.swift
@@ -0,0 +1,36 @@
+//
+//  DeviceRotationViewModifier.swift
+//  ClueDetectiveNotes
+//
+//  Created by Dasan on 7/8/24.
+//
+
+import SwiftUI
+
+struct DeviceRotationViewModifier: ViewModifier {
+    let action: (UIDeviceOrientation) -> Void
+    
+    func body(content: Content) -> some View {
+        content
+            .onAppear()
+            .onReceive(NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)) { _ in
+                action(UIDevice.current.orientation)
+            }
+    }
+}
+
+extension View {
+    func onRotate(perform action: @escaping (UIDeviceOrientation) -> Void) -> some View {
+        self.modifier(DeviceRotationViewModifier(action: action))
+    }
+}
+
+extension UIApplication {
+    // UIWindow 추출
+    public var keyWindow: UIWindow? {
+        connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .flatMap { $0.windows }
+            .first { $0.isKeyWindow }
+    }
+}
