18e38aea96ae3f49df6ded677ef385745466b750
DasanKim <tempzipp@gmail.com>
2024-07-18 16:07:39 +0900

    ♻️Refactor: CardType, ClueCard, Cards 타입 수정
    - CardType: none 타입 추가
    - ClueCard 객체에 rawName 프로퍼티 추가
    - Cards: allCardsCount, getCards 메서드 추가

--- ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Card/Cards.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Card/Cards.swift
@@ -9,6 +9,7 @@ enum CardType: CustomStringConvertible {
     case suspect
     case weapon
     case room
+    case none
     
     var description: String {
         switch self {
@@ -18,21 +19,41 @@ enum CardType: CustomStringConvertible {
             return "무기"
         case .room:
             return "장소"
+        case .none:
+            return "빈값"
         }
     }
 }
 
 struct ClueCard: Hashable {
+    let rawName: String
     let name: String
     let type: CardType
 }
 
 struct Cards {
     let suspects: [ClueCard]
-    let weapon: [ClueCard]
-    let room: [ClueCard]
+    let weapons: [ClueCard]
+    let rooms: [ClueCard]
     
     func allCards() -> [ClueCard] {
-        return suspects + weapon + room
+        return suspects + weapons + rooms
+    }
+    
+    func allCardsCount() -> Int {
+        return suspects.count + weapons.count + rooms.count
+    }
+    
+    func getCards(type: CardType) -> [ClueCard] {
+        switch type {
+        case .suspect:
+            return suspects
+        case .weapon:
+            return weapons
+        case .room:
+            return rooms
+        default:
+            return []
+        }
     }
 }
