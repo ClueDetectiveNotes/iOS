14fff58bf4a0aecfd25444a1ebcb02416a5e3162
DasanKim <tempzipp@gmail.com>
2024-07-18 16:13:00 +0900

    ✨Feat: 프리뷰를 위한 Dummy 객체 생성
    - DummyPlayers, DummySetting 객체 생성

--- ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Game/GameSetter.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Game/GameSetter.swift
@@ -18,16 +18,7 @@ final class GameSetter {
             return sheet
         } else {
             let newSheet = Sheet(
-                players: //setting.getPlayers(),
-                [
-                    User(id: 1, name: "코코"),
-                    Other(id: 2, name: "다산"),
-                    Other(id: 3, name: "메리"),
-                    Other(id: 4, name: "야곰"),
-                    Other(id: 5, name: "가가"),
-                    Other(id: 6, name: "나나"),
-                    Solution(id: 10000, name: "정답")
-                ],
+                players: setting.getPlayers(),
                 cards: setting.getEdition().cards
             )
             self.sheet = newSheet
@@ -37,5 +28,32 @@ final class GameSetter {
     
     func getSetting() -> Setting {
         return setting
+        //return DummySetting.setting
     }
+}
+
+struct DummyPlayers {
+    static let players: [any Player] = [
+        User(id: 1, name: "코코"),
+        Other(id: 2, name: "다산"),
+        Other(id: 3, name: "메리"),
+        Other(id: 4, name: "야곰"),
+        Other(id: 5, name: "가가"),
+        Other(id: 6, name: "나나"),
+        Solution(id: 10000, name: "정답")
+    ]
+}
+
+struct DummySetting {
+    static let setting: Setting = Setting(
+        players: DummyPlayers.players,
+        edition: .classic,
+        subMarkerTypes: [
+            SubMarker(notation: "1"),
+            SubMarker(notation: "2"),
+            SubMarker(notation: "3"),
+            SubMarker(notation: "4")
+        ],
+        publicCards: []
+    )
 }
