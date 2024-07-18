f2c9ca247f69cec63925e7bb2b14e7354245dab7
DasanKim <tempzipp@gmail.com>
2024-07-18 16:09:42 +0900

    ♻️Refactor: ClueCard 타입 수정에 따른 변경 및 기타 수정
    - 사용하지 않는 코드 주석처리

--- ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Card/Card.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Domain/Model/Card/Card.swift
@@ -5,14 +5,16 @@
 //  Created by Dasan & Mary on 2024/04/13.
 //
 
-struct Card: Hashable {
-    let owner: (any Player)?
-    
-    static func == (lhs: Card, rhs: Card) -> Bool {
-        return lhs.owner?.id == rhs.owner?.id
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(owner?.id)
-    }
-}
+//struct Card {
+//    let owner: (any Player)?
+//}
+//
+//extension Card: Hashable {
+//    static func == (lhs: Card, rhs: Card) -> Bool {
+//        return lhs.owner?.id == rhs.owner?.id
+//    }
+//    
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(owner?.id)
+//    }
+//}
