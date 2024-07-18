62fb8fed0b839f7a75964d41aee52a1fa0ce6b07
DasanKim <tempzipp@gmail.com>
2024-07-18 16:26:53 +0900

    ✨Feat: MyCardsSettingView 생성

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/View/MyCardsSettingView.swift
@@ -0,0 +1,29 @@
+//
+//  MyCardsSettingView.swift
+//  ClueDetectiveNotes
+//
+//  Created by Yena on 7/18/24.
+//
+
+import SwiftUI
+
+struct MyCardsSettingView: View {
+//    @ObservedObject private var settingStore: SettingStore
+//    private var settingInteractor: SettingInteractor
+//    
+//    init(
+//        settingStore: SettingStore,
+//        settingInteractor: SettingInteractor
+//    ) {
+//        self.settingStore = settingStore
+//        self.settingInteractor = settingInteractor
+//    }
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyCardsSettingView()
+}
