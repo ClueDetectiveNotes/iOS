7d5fde839de2756be51cd99653e2b18eb6654352
DasanKim <tempzipp@gmail.com>
2024-10-30 22:59:38 +0900

    ✨Feat: OptionStore, OptionIntent 생성

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/Store/OptionStore.swift
@@ -0,0 +1,36 @@
+//
+//  OptionStore.swift
+//  ClueDetectiveNotes
+//
+//  Created by Yena on 10/30/24.
+//
+
+import Foundation
+
+final class OptionStore: ObservableObject {
+    @Published var language: Language
+    @Published var screenMode: ScreenMode
+    @Published var autoAnswerMode: AutoAnswerMode
+    
+    init(
+        language: Language = .korean,
+        screenMode: ScreenMode = .system,
+        autoAnswerMode: AutoAnswerMode = .off
+    ) {
+        self.language = language
+        self.screenMode = screenMode
+        self.autoAnswerMode = autoAnswerMode
+    }
+    
+    func setLanguage(_ language: Language) {
+        self.language = language
+    }
+    
+    func setScreenMode(_ screenMode: ScreenMode) {
+        self.screenMode = screenMode
+    }
+    
+    func setAutoAnswerMode(_ autoAnswerMode: AutoAnswerMode) {
+        self.autoAnswerMode = autoAnswerMode
+    }
+}
