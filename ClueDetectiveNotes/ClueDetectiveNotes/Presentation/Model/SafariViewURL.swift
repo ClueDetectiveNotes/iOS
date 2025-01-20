7f7411cc3f19de829b288afdfb206af06eac17ba
DasanKim <tempzipp@gmail.com>
2025-01-21 00:00:34 +0900

    ✨Feat: WebView를 위한 SafariView, SafariViewURL 생성

---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/Model/SafariViewURL.swift
@@ -0,0 +1,17 @@
+//
+//  SafariViewURL.swift
+//  ClueDetectiveNotes
+//
+//  Created by Dasan on 1/20/25.
+//
+
+enum SafariViewURL {
+    case helpView
+    
+    var url: String {
+        switch self {
+        case .helpView:
+            return "https://cyan-satin-9e6.notion.site/181cb7d0177180358bfeca9764fb2e42?pvs=4"
+        }
+    }
+}
---
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/View/SafariView.swift
@@ -0,0 +1,21 @@
+//
+//  SafariView.swift
+//  ClueDetectiveNotes
+//
+//  Created by Dasan on 1/20/25.
+//
+
+import SwiftUI
+import SafariServices
+
+struct SafariView: UIViewControllerRepresentable {
+    let url: URL
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<SafariView>) -> SFSafariViewController {
+        return SFSafariViewController(url: url)
+    }
+    
+    func updateUIViewController(_ uiViewController: SFSafariViewController, context: UIViewControllerRepresentableContext<SafariView>) {
+        
+    }
+}
