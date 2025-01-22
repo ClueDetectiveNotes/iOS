e0f62d0123eda53ee61804e85f41192b24273c85
DasanKim <tempzipp@gmail.com>
2025-01-22 11:05:49 +0900

    🩹Chore: 앱 내 url을 관리하는 열거형 SafariViewURL의 네이밍을 WebPages으로 수정

--- ClueDetectiveNotes/ClueDetectiveNotes/Presentation/Model/WebPages.swift
+++ ClueDetectiveNotes/ClueDetectiveNotes/Presentation/Model/WebPages.swift
@@ -1,16 +1,16 @@
 //
-//  SafariViewURL.swift
+//  WebPages.swift
 //  ClueDetectiveNotes
 //
 //  Created by Dasan on 1/20/25.
 //
 
-enum SafariViewURL {
-    case helpView
+enum WebPages {
+    case help
     
     var url: String {
         switch self {
-        case .helpView:
+        case .help:
             return "https://cyan-satin-9e6.notion.site/181cb7d0177180358bfeca9764fb2e42?pvs=4"
         }
     }
